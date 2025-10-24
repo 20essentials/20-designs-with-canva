63aca7b6c02c9379d769fd64f6f24959c8b03e7e
DominicDev <s0l0estudi4r@gmail.com>
2025-10-25 00:44:22 +0200

    Updates

---
+++ static/ContainerCards.astro_astro_type_script_index_0_lang.DpHMhbbp.js
@@ -0,0 +1,1 @@
+import{$ as o}from"./functions.BAgsSORx.js";const n=o(".am-slider");document.addEventListener("mousemove",e=>{const t=e.clientX/4;n.style.transform=`rotateY(${t}deg)`});
---
+++ static/functions.BAgsSORx.js
@@ -0,0 +1,1 @@
+const t=e=>document.querySelector(e);function n(e){return new URL(e.replace(/^\/+/,""),"https://20essentials.github.io/20-designs-with-canva/").toString()}export{t as $,n as b};
